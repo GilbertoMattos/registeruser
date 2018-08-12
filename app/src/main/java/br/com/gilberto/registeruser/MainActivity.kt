0fb85fcbf41194715a52631ca4f4341162eae0c0
GilbertoMatos <gmc.beto@gmail.com>
2018-08-12 17:08:05 -0300

    navegando entre activitys passando valores entre elas

--- app/src/main/java/br/com/gilberto/registeruser/MainActivity.kt
+++ app/src/main/java/br/com/gilberto/registeruser/MainActivity.kt
@@ -1,8 +1,11 @@
 package br.com.gilberto.registeruser
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.TextInputLayout
@@ -11,8 +14,8 @@ import android.support.v7.widget.AppCompatEditText
 import android.support.v7.widget.AppCompatSpinner
 import android.support.v7.widget.Toolbar
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.TimePicker
+import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 import br.com.gilberto.registeruser.model.Pessoa
 import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDate
@@ -27,9 +30,6 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
     private lateinit var inputLay_email: TextInputLayout
     private lateinit var inputLay_senha: TextInputLayout
     private lateinit var inputLay_repete_senha: TextInputLayout
-    private lateinit var inputLay_sexo: TextInputLayout
-    private lateinit var inputLay_profissao: TextInputLayout
-    private lateinit var inputLay_estadoCivil: TextInputLayout
 
     private lateinit var edit_nome_completo: AppCompatEditText
     private lateinit var edit_email: AppCompatEditText
@@ -42,7 +42,12 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
 
     private lateinit var toolbar: Toolbar
 
-    private var dataNascimento : LocalDate? = null
+    private lateinit var profissoes: ArrayList<String>
+    private lateinit var sexos: ArrayList<String>
+    private lateinit var estadosCivis: ArrayList<String>
+
+    private var dataNascimento: LocalDate? = null
+    private lateinit var context: Context
 
     companion object {
         val activity_result: Int = 99
@@ -59,15 +64,17 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        context = this
+
+        profissoes = arrayListOf("Selecione", "Adicionar Nova", "Desenvolvedor", "Analista", "Ator")
+        sexos = arrayListOf("Selecine", "Mascunilo", "Feminino")
+        estadosCivis = arrayListOf("Selecine", "Solteiro", "Casado", "Divorciado")
+
         inputLay_nome_completo = findViewById(R.id.til_nome_completo)
         inputLay_data_nascimento = findViewById(R.id.til_data_nascimento)
         inputLay_email = findViewById(R.id.til_email)
         inputLay_repete_senha = findViewById(R.id.til_repeat_senha)
         inputLay_senha = findViewById(R.id.til_senha)
-        inputLay_sexo = findViewById(R.id.til_sexo)
-        inputLay_profissao = findViewById(R.id.til_profissao)
-        inputLay_estadoCivil = findViewById(R.id.til_estadocivil)
-
 
         edit_nome_completo = findViewById(R.id.edt_name_completo)
         edit_email = findViewById(R.id.edt_email)
@@ -82,14 +89,30 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
 
         populationSpiners()
 
+        spinner_profissao.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(parentView: AdapterView<*>, selectedItemView: View, position: Int, id: Long) {
+                if (position == 1) {
+                    //Adicionar nova profissa
+                    dialogNovaProfissao()
+                }
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>) {
+
+            }
+
+        }
+
         btn_salvar.setOnClickListener {
-            validarForm()
 
-            val pessoa = popularObjeto()
+            if (validarForm()) {
 
-            val intent = Intent(this, ResultActivity::class.java)
-            intent.putExtra("pessoa", pessoa)
-            startActivityForResult(intent, activity_result)
+                val pessoa = popularObjeto()
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("pessoa", pessoa)
+                startActivityForResult(intent, activity_result)
+            }
         }
 
         edit_data_nascimento.setOnClickListener {
@@ -101,6 +124,24 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
                 selectDate()
             }
         }
+    }
+
+    private fun dialogNovaProfissao() {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Nova Profissão")
+        builder.setCancelable(false)
+
+        val input = EditText(context)
+        input.hint = "Informe o noma da nova profissão"
+        builder.setView(input)
+
+        builder.setPositiveButton(android.R.string.ok, DialogInterface.OnClickListener { dialog, which ->
+            profissoes.add(input.text.toString())
+            spinner_profissao.setSelection(profissoes.size - 1)
+        })
+        builder.setNegativeButton(android.R.string.cancel, DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
+
+        builder.show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -123,9 +164,9 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
         edit_senha.setText(pessoa.senha)
         edit_repete_senha.setText(pessoa.repeteSenha)
         edit_data_nascimento.setText(format.format(pessoa.nascimento))
-        spinner_profissao.setSelection(1)
-        spinner_estadoCivil.setSelection(1)
-        spinner_sexo.setSelection(1)
+        spinner_profissao.setSelection(profissoes.indexOf(pessoa.profissao))
+        spinner_estadoCivil.setSelection(estadosCivis.indexOf(pessoa.estadoCivil))
+        spinner_sexo.setSelection(sexos.indexOf(pessoa.sexo))
 
     }
 
@@ -162,7 +203,22 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
 
 
         val datePickerDialog = DatePickerDialog(this,
-                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth -> edit_data_nascimento.setText("$dayOfMonth/${monthOfYear + 1}/$year") }, mYear, mMonth, mDay)
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    var text = ""
+                    text += if (dayOfMonth < 10) {
+                        "0$dayOfMonth/"
+                    } else {
+                        "$dayOfMonth/"
+                    }
+                    text += if (monthOfYear < 10) {
+                        "0$monthOfYear/"
+                    } else {
+                        "$monthOfYear/"
+                    }
+
+                    text += "$year"
+                    edit_data_nascimento.setText("$text")
+                }, mYear, mMonth, mDay)
         datePickerDialog.show()
     }
 
@@ -174,51 +230,66 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
 
     private fun populationSpiners() {
 
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sexoArray())
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sexos)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner_sexo.adapter = adapter
 
-        val adapterEstadoCivil = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, estadoCivilArray())
+        val adapterEstadoCivil = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, estadosCivis)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner_estadoCivil.adapter = adapterEstadoCivil
 
-        val adapterProfissao = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, profissaoArray())
+        val adapterProfissao = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, profissoes)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner_profissao.adapter = adapterProfissao
     }
 
-    fun sexoArray(): ArrayList<String> {
-        return arrayListOf("Mascunilo", "Feminino")
-    }
+    private fun validarForm(): Boolean {
 
-    fun estadoCivilArray(): ArrayList<String> {
-        return arrayListOf("Solteiro", "Casado", "Divorciado")
-    }
-
-    fun profissaoArray(): ArrayList<String> {
-        return arrayListOf("Desenvolvedor", "Analista", "Ator")
-    }
-
-    private fun validarForm() {
+        var validado = true
 
         if (edit_nome_completo.text.toString().isEmpty()) {
             inputLay_nome_completo.isErrorEnabled = true
             inputLay_nome_completo.error = "Por favor, preencher o campo nome completo"
+            validado = false
         } else {
             inputLay_nome_completo.isErrorEnabled = false
+        }
+
+        if (edit_data_nascimento.text.toString().isEmpty()) {
+            inputLay_data_nascimento.isErrorEnabled = true
+            inputLay_data_nascimento.error = "Por favor, preencher o campo data de nascimento"
+            validado = false
+        } else {
+            inputLay_data_nascimento.isErrorEnabled = false
         }
 
         if (edit_email.text.toString().isEmpty()) {
             inputLay_email.isErrorEnabled = true
             inputLay_email.error = "Por favor, preencher o campo email"
+            validado = false
         } else {
             inputLay_email.isErrorEnabled = false
         }
 
+        if (spinner_sexo.selectedItemPosition == 0) {
+            Toast.makeText(context, "Por favor, preencher o campo sexo", Toast.LENGTH_LONG).show()
+            validado = false
+        }
+
+        if (spinner_profissao.selectedItemPosition == 0 || spinner_profissao.selectedItemPosition == 1) {
+            Toast.makeText(context, "Por favor, preencher o campo profissão", Toast.LENGTH_LONG).show()
+            validado = false
+        }
+
+        if (spinner_estadoCivil.selectedItemPosition == 0) {
+            Toast.makeText(context, "Por favor, preencher o campo estado civil", Toast.LENGTH_LONG).show()
+            validado = false
+        }
 
         if (edit_senha.text.toString().isEmpty()) {
             inputLay_senha.isErrorEnabled = true
             inputLay_senha.error = "Por favor, preencher o campo senha"
+            validado = false
         } else {
             inputLay_senha.isErrorEnabled = false
         }
@@ -227,6 +298,7 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
         if (edit_repete_senha.text.toString().isEmpty()) {
             inputLay_repete_senha.isErrorEnabled = true
             inputLay_repete_senha.error = "Por favor, confirmar a senha"
+            validado = false
         } else {
             inputLay_repete_senha.isErrorEnabled = false
         }
@@ -235,9 +307,20 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
             if (edit_senha.text.toString() != edit_repete_senha.text.toString()) {
                 inputLay_repete_senha.isErrorEnabled = true
                 inputLay_repete_senha.error = "As senhas informadas não conferem"
+                validado = false
             } else {
                 inputLay_repete_senha.isErrorEnabled = false
             }
         }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(edit_email.text.toString()).matches()) {
+            inputLay_email.isErrorEnabled = true
+            inputLay_email.error = "O email informado é invalido"
+            validado = false
+        } else {
+            inputLay_email.isErrorEnabled = false
+        }
+
+        return validado
     }
 }
